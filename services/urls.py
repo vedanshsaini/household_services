44cc0ebb6f2275c3d476edddd2dbd679984e3d98
VEDANSH SAINI <vedanshsaini786@gmail.com>
2024-05-05 22:57:17 +0530

    urls changes

--- services/urls.py
+++ services/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
+    path('', views.register, name='register'),
     path('services/', views.list_services, name='list_services'),
     path('place-order/', views.place_order, name='place_order'),
     path('process-payment/<int:order_id>/', views.process_payment, name='process_payment'),
